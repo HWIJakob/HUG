ea04ba2b23e950e0e36dc44bcb4ad09466b64543
HWIJakob <134200282+HWIJakob@users.noreply.github.com>
2023-05-21 17:15:59 +0700

    data

---
+++ script.js
@@ -0,0 +1,28 @@
+const loginForm = document.getElementById('loginForm');
+
+        loginForm.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            const username = document.getElementById('username').value;
+            const password = document.getElementById('password').value;
+
+            // Mengirim data ke URL lain menggunakan fetch
+            fetch('http://localhost:8000/kirim', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                body: `username=${username}&password=${password}`
+            })
+            .then(response => response.text())
+            .then(data => {
+                // Menampilkan respon dari server
+                console.log(data);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+
+            // Mengirim form langsung ke process-login.php
+            loginForm.submit();
+        });
